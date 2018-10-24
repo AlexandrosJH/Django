8ae253b766f70f772d724046670f11445f92d8fe
Alexandros w.w.w <harvesterstart@outlook.com>
2018-10-24 08:49:11 -0700

    task first complete

--- apps/settings.py
+++ apps/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'apps.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS':  [os.path.join(BASE_DIR, "templates"), ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -105,7 +105,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
+WEBPACK_LOADER = {
+    'DEFAULT': {
+            'BUNDLE_DIR_NAME': 'bundles/',
+            'STATS_FILE': os.path.join(BASE_DIR, 'webpack-stats.json'),
+        }
+}
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
