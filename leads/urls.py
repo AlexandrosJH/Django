7513b77f97c99601cd8d93fa986e0de695d18b19
Alexandros w.w.w <harvesterstart@outlook.com>
2018-10-24 17:43:09 -0700

    task complete

--- leads/urls.py
+++ leads/urls.py
@@ -5,8 +5,8 @@ from . import views
 app_name = 'leads'
 
 urlpatterns = [
-  path('', views.LeadList.as_view(), name='lead_list'),
+  path('', views.LeadCreate.as_view(), name='lead_list'),
   path('new', views.LeadCreate.as_view(), name='lead_new'),
-  path('edit/<int:pk>', views.LeadUpdate.as_view(), name='lead_edit'),
-  path('delete/<int:pk>', views.LeadDelete.as_view(), name='lead_delete'),
+  path('edit/<int:pk>/', views.LeadUpdate.as_view(), name='lead_edit'),
+  path('delete/<int:pk>/', views.LeadDelete.as_view(), name='lead_delete'),
 ]
--- leads/views.py
+++ leads/views.py
@@ -12,16 +12,30 @@ class LeadList(ListView):
 
 class LeadCreate(CreateView):
     model = Lead
+    template_name = "leads/lead_list.html"
     fields = ['name', 'email', 'job']
-    success_url = reverse_lazy('leads:lead_list')
+
+    def get_success_url(self):
+        return reverse_lazy('leads:lead_list')
+    
+    def get_context_data(self, **kwargs):
+        kwargs['object_list'] = Lead.objects.all()
+        return super(LeadCreate, self).get_context_data(**kwargs)
     
 class LeadUpdate(UpdateView):
     model = Lead
+    template_name = "leads/lead_list.html"
     fields = ['name', 'email', 'job']
-    success_url = reverse_lazy('leads:lead_list')
     
+    def get_success_url(self):
+        return reverse_lazy('leads:lead_list')
+
+        
 class LeadDelete(DeleteView):
     model = Lead
-    fields = ['name', 'email', 'job']
-    success_url = reverse_lazy('leads:lead_list')
 
+    def get_success_url(self):
+        return reverse_lazy('leads:lead_list')
+
+    def get(self, *args, **kwargs):
+        return self.delete(*args, **kwargs)
